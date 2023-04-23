4407fd00caf5b0bc5466d40f4adbcc211b4b1674
Dudus Jestem <dudak799@gmail.com>
2023-04-23 18:19:10 +0200

    New site structure, added more parameters

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+	Serial.begin(115200);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+	Serial.println("Nigger");
 }
